40899f3c5c8a09c9a215791d33a44f7e94f25e71
Carlos Machado <carlosmachado@edu.univali.br>
2021-05-11 05:59:10 -0300

    starting vm

---
+++ src/br/univali/ttoproject/vm/VirtualMachine.java
@@ -0,0 +1,13 @@
+package br.univali.ttoproject.vm;
+
+import java.util.ArrayList;
+
+public class VirtualMachine {
+    private ArrayList<Object> stack;
+    private int pointer;
+    private int top;
+
+    private void add() {
+        
+    }
+}
